80171778046f349d765fdc8a25d870f4b3c2d599
Jesica Hoffman <jesica_889@hotmail.com>
2023-02-26 19:53:12 -0300

    close-info

--- src/app/components/works/works.component.ts
+++ src/app/components/works/works.component.ts
@@ -21,11 +21,30 @@ const button = (document.querySelector('.companies-min') as HTMLButtonElement);
 const info = (document.querySelector('.companies') as HTMLElement);
 const buttonCompanies = (document.querySelector('.button-companies') as HTMLButtonElement);
 
+const button1 = (document.querySelector('.companies-min1') as HTMLButtonElement);
+const info1 = (document.querySelector('.companies1') as HTMLElement);
+const buttonCompanies1 = (document.querySelector('.button-companies1') as HTMLButtonElement);
+
+const button2 = (document.querySelector('.companies-min2') as HTMLButtonElement);
+const info2 = (document.querySelector('.companies2') as HTMLElement);
+const buttonCompanies2 = (document.querySelector('.button-companies2') as HTMLButtonElement);
+
+info.classList.remove('inactivo');
+  info.classList.toggle('activo');
+  button.classList.toggle('inactivo');
+
 button.addEventListener('click', ():void => {
   info.classList.remove('inactivo');
   info.classList.toggle('activo');
   button.classList.toggle('inactivo');
 
+  button1.classList.remove('inactivo');
+  button1.classList.toggle('activo');
+  info1.classList.remove('activo');
+
+  button2.classList.remove('inactivo');
+  button2.classList.toggle('activo');
+  info2.classList.remove('activo');
 });
 
 buttonCompanies.addEventListener('click', ():void => {
@@ -35,14 +54,20 @@ buttonCompanies.addEventListener('click', ():void => {
 
 });
 
-const button1 = (document.querySelector('.companies-min1') as HTMLButtonElement);
-const info1 = (document.querySelector('.companies1') as HTMLElement);
-const buttonCompanies1 = (document.querySelector('.button-companies1') as HTMLButtonElement);
+
 
 button1.addEventListener('click', ():void => {
   info1.classList.remove('inactivo');
   info1.classList.toggle('activo');
   button1.classList.toggle('inactivo');
+
+  button2.classList.remove('inactivo');
+  button2.classList.toggle('activo');
+  info2.classList.remove('activo');
+
+  button.classList.remove('inactivo');
+  button.classList.toggle('activo');
+  info.classList.remove('activo');
 
 });
 
@@ -53,14 +78,20 @@ buttonCompanies1.addEventListener('click', ():void => {
 
 });
 
-const button2 = (document.querySelector('.companies-min2') as HTMLButtonElement);
-const info2 = (document.querySelector('.companies2') as HTMLElement);
-const buttonCompanies2 = (document.querySelector('.button-companies2') as HTMLButtonElement);
+
 
 button2.addEventListener('click', ():void => {
   info2.classList.remove('inactivo');
   info2.classList.toggle('activo');
   button2.classList.toggle('inactivo');
+
+  button.classList.remove('inactivo');
+  button.classList.toggle('activo');
+  info.classList.remove('activo');
+
+  button1.classList.remove('inactivo');
+  button1.classList.toggle('activo');
+  info1.classList.remove('activo');
 
 });
 
